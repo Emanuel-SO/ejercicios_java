93014bb43e24d9ff388245eda7a322aa35112229
Emanuel Saucedo <emanuelsaucedo52@gmail.com>
2023-03-08 14:07:08 -0600

    creacion del archivo de los ejercicios Java

---
+++ ejercicios01.java
@@ -0,0 +1,14 @@
+package practica1;
+
+public class Practica1 {
+	
+	static void primerMetodo(int x) {
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		primerMetodo(134);
+
+	}
+
+}
